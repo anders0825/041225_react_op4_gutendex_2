2e82e3b81120d5919e9ccf33935d38f4330a7011
anders0825 <gah082025@gmail.com>
2026-01-14 18:24:31 +0100

    package.json update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/041225_react_op4_gutendex_2/",
